41d53fdf780e2d9e997dc6136105eb14e9dd48e0
truongdinhduc <ducdinhtruong712@gmail.com>
2021-05-09 17:48:04 +0700

    My shopping website

---
+++ Shopping_website/script.js
@@ -0,0 +1,15 @@
+var mainImg=document.getElementById("main-img");
+var suprtImg=document.getElementsByClassName("support-img");
+suprtImg[0].onclick=function(){
+    mainImg.src=suprtImg[0].src;
+}
+suprtImg[1].onclick=function(){
+    mainImg.src=suprtImg[1].src;
+}
+suprtImg[2].onclick=function(){
+    mainImg.src=suprtImg[2].src;
+}
+suprtImg[3].onclick=function(){
+    mainImg.src=suprtImg[3].src;
+}
+
